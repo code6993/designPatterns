82812fe7457802ba575cc530cad9a045a8139f11
code6993 <68768875+code6993@users.noreply.github.com>
2021-01-04 13:54:26 +0530

    Update MaxSumSubArrayOfSizeK.java

--- DesignPatternCodeSample/src/com/competetive/prg/MaxSumSubArrayOfSizeK.java
+++ DesignPatternCodeSample/src/com/competetive/prg/MaxSumSubArrayOfSizeK.java
@@ -2,10 +2,11 @@ package com.competetive.prg;
 
 public class MaxSumSubArrayOfSizeK {
 
+	//sliding window easy problem 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] arr = {1,0,2,3};
-		int sum = solve(arr,2);
+		int[] arr = {2, 1, 5, 1, 3, 2};
+		int sum = solve(arr,3);
 		System.out.print(sum);
 	}
 	
